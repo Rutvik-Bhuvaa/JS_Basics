c49c6a1436603785f31584f73b23370e2a265e9c
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-11-29 13:21:38 +0530

    Added example of increment and decrement operators

---
+++ operators.js
@@ -0,0 +1,9 @@
+// 1. Increment and Decrement operator
+
+let num = 20;
+// post increment and decrement (first print and then increment/decrement)
+console.log(num++);
+console.log(num--);
+// pre increment and decrement (first increment/decrement and then print)
+console.log(++num);
+console.log(--num);
