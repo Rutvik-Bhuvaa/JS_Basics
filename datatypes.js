8ac02eb0b17931a5f33196b027bb559fc50b82b5
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-11-29 07:14:58 +0530

    Example of number and string datatype added

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+// Declare the data-type
+let num = 10;
+console.log(num);
+
+// Primitive data-types
+/**
+ * Number
+ * Boolean
+ * String
+ * Null
+ * Undefined
+ * BigInt
+ * Symbol
+ */
+
+// Example of number data-type
+let account_balance = 10;
+console.log(account_balance);
+console.log(typeof account_balance);
+
+// Example of string data-type
+let str = "xyz is a good boy and also he doesn't know how to use zoom";
+console.log(str);
+console.log(typeof str);
+
+// Example of data-type
