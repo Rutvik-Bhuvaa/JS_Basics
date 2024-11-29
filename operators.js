cf3dfba165f163cff289a4f2b0b8b6a12789707e
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-11-29 13:25:02 +0530

    Added assignment operator example

--- operators.js
+++ operators.js
@@ -1,5 +1,4 @@
 // 1. Increment and Decrement operator
-
 let num = 20;
 // post increment and decrement (first print and then increment/decrement)
 console.log(num++);
@@ -7,3 +6,7 @@ console.log(num--);
 // pre increment and decrement (first increment/decrement and then print)
 console.log(++num);
 console.log(--num);
+
+// 2. Assignment operator
+let numTwo = 10; // = is used for assign value to numTwo
+console.log(numTwo);
