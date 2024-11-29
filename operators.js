6b2e9cc838c2742fe1463af18946d074f617a4b4
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-11-29 14:00:45 +0530

    Added Comparison operator example

--- operators.js
+++ operators.js
@@ -10,3 +10,38 @@ console.log(--num);
 // 2. Assignment operator
 let numTwo = 10; // = is used for assign value to numTwo
 console.log(numTwo);
+
+// 3. Comparison operator
+
+// number to number
+let a1 = 1;
+let a2 = 2;
+console.log(a1 == a2);
+console.log(a1 > a2);
+console.log(a1 < a2);
+console.log(a1 <= a2);
+console.log(a1 >= a2);
+
+let digit = 10;
+let str = "10";
+console.log(digit == str); // true => Here first string convert into the number then comparison begins
+
+// null == undefined --> true otherwise false
+console.log(null == undefined);
+// null === undefined --> false
+console.log(null === undefined);
+// null can be only equivalent to undefined
+console.log(null == 0); // false
+console.log(null > 0); // false
+console.log(null < 0); // false
+console.log(null >= 0); // true
+console.log(null <= 0); // true
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+
+// NaN is never equal to NaN
+console.log(NaN == NaN);
