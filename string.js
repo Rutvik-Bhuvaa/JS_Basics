c4489fab10ad65b6defbf2d1530990b93c572d13
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-11-30 16:50:02 +0530

    Added the basic methods to define strings in JS

---
+++ string.js
@@ -0,0 +1,8 @@
+let strOne = "Hi there";
+let strTwo = "Kaise ho sab";
+let price = 50;
+let strThree = `Price of the tomato is ${price}`;
+
+console.log(strOne);
+console.log(strTwo);
+console.log(strThree);
