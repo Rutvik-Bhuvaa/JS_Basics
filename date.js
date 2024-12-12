5920a21bd90c928f02216d1c2608a75a288685ec
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-12-12 07:17:53 +0530

    Added some more examples on date

--- date.js
+++ date.js
@@ -10,3 +10,14 @@ console.log(d.getDate());
 console.log(d.getDay());
 console.log(d.getMonth());
 console.log(d.getFullYear());
+
+// Number: 0 based start
+// String: 1 based start
+console.log(d.getTime());
+const now = Date.now();
+console.log(now);
+d.setDate(1);
+d.setMonth(12);
+d.setFullYear(2023);
+console.log(d);
+console.log(d.toLocaleDateString());
