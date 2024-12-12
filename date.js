3fdb2bf9a097db52a0c6d28f144a3cbbf911395d
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-12-12 07:05:05 +0530

    Added some date function

---
+++ date.js
@@ -0,0 +1,12 @@
+const d = new Date();
+console.log(d);
+console.log(d.toDateString());
+console.log(d.toString());
+console.log(d.toISOString());
+
+console.log(typeof d);
+
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getMonth());
+console.log(d.getFullYear());
