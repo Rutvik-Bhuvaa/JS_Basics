eb65ebfcbe11c41507fb115b65cc4649d4c248fa
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-12-17 20:07:25 +0530

    Something added related to object

---
+++ object.js
@@ -0,0 +1,14 @@
+// how to create object
+
+// key of the object store as a string
+const obj = {
+  firstName: "RutvikBhuvaa",
+  accountBalance: 420,
+  gender: "Male",
+  age: 30,
+};
+
+console.log(obj);
+// to retrieve the properties of the object
+console.log(obj.gender);
+console.log(obj["gender"]);
