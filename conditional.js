5c5871e42455c89b3b7aa399de61cac2fe187cd6
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-12-29 06:33:28 +0530

    Added exmple of if-else and if-else-if

---
+++ conditional.js
@@ -0,0 +1,13 @@
+// if-else
+
+let age = 7;
+
+if (age >= 18) console.log("Eligible for vote");
+else console.log("Not eligible for vote");
+
+// if-else-if
+let ageOf = 19;
+
+if (ageOf < 18) console.log("KID");
+else if (ageOf > 45) console.log("OLD");
+else console.log("YOUNG");
