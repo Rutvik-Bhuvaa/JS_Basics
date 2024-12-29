7a52f02d70b5a1f9e1b7dad4100dfa20f259460b
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-12-29 06:43:14 +0530

    Added the example of switch

--- conditional.js
+++ conditional.js
@@ -11,3 +11,32 @@ let ageOf = 19;
 if (ageOf < 18) console.log("KID");
 else if (ageOf > 45) console.log("OLD");
 else console.log("YOUNG");
+
+// switch
+let day = 4;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Not a valid day");
+}
